8f255b2d85d6b67608b93bd147a56fc0d1be1763
Xiaying Peng <xiaying.peng@coinbase.com>
2022-11-18 16:06:44 -0800

    Delete cli.sh

--- .github/scripts/cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# downloading cli
-curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
-
-echo "start check:data"
-./bin/rosetta-cli --configuration-file examples/ethereum/rosetta-cli-conf/devnet/config.json check:data --start-block 0
